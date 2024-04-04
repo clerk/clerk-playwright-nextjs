8bde1aea614cbb060a74895080ce9df78eac1108
Stefanos Anagnostou <stefanos@clerk.dev>
2024-04-04 19:58:46 +0300

    Add Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <SignIn />
+    </div>
+  );
+}
