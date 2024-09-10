4c864651343e7a39b5123ae3ae86279e9ecec74f
victoriasiderea <git@victoriasiderea.com>
2024-09-10 13:29:04 -0400

    update regex for config

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-    '/protected(.*)',
-  ]);
- 
-export default clerkMiddleware((auth, req) => {
+const isProtectedRoute = createRouteMatcher(["/protected(.*)"]);
+
+export default clerkMiddleware(
+  (auth, req) => {
     if (isProtectedRoute(req)) auth().protect();
-}, {
-  signInUrl: '/sign-in',
-  signUpUrl: '/sign-up',
-});
- 
+  },
+  {
+    signInUrl: "/sign-in",
+    signUpUrl: "/sign-up",
+  }
+);
+
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
